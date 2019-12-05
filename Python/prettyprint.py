f7cdadac115210d3eed87d7e17bc57555baed3c9
Truls H <fanthoft@gmail.com>
2019-12-05 13:27:30 +0100

    Solved day 5!

--- Python/prettyprint.py
+++
@@ -1,6 +0,0 @@
-
-def pprint(day, p1, p1message, p2, p2message):
-    print(f" Day {day} ".center(64, "-"))
-    print(f"{p1message}: {p1}".center(64))
-    print(f"{p2message}: {p2}".center(64))
-    print("".center(64, "-"))
